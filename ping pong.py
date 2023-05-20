d11b576f791bcebe5653e4c1f5631f5e9d877589
Quat <132725936+QuaterFL@users.noreply.github.com>
2023-05-20 11:10:24 +0800

    Update ping pong.py

--- ping pong.py
+++ ping pong.py
@@ -1,35 +1,70 @@
+from pygame import *
+font.init()
+#from random import randint
+
+window = display.set_mode((900, 450))
+display.set_caption("Пинг Понг")
+background = transform.scale(image.load("field.png"), (900, 450))
+
+
 class GameSprite(sprite.Sprite):
-#создание класса
     def __init__(self, player_image, player_x, player_y, player_speed, width, hight):
-    #создание функции для характеристик
         super().__init__()
-        #для характеристик
         self.image = transform.scale(image.load(player_image), (width, hight))
-        #задать картинку
         self.speed = player_speed
-        #задать скорость
         self.rect = self.image.get_rect()
-        #задать прямоугольник в который вписан спрайт
         self.rect.x = player_x
-        #задать координаты x
         self.rect.y = player_y
-        #задать координаты y
     def reset(self):
-    #создание функции для отображения
         window.blit(self.image, (self.rect.x, self.rect.y))
-        #отображение картинки
 
 class Player(GameSprite):
-#создание класса
     def update(self):
-    #функция для пердвижения
         keys_pressed = key.get_pressed()
-        #получение событии клавиатуры
-        if keys_pressed[K_RIGHT] and self.rect.x <650:
-        #если нажата кнопка право и координаты меньше 650
-            self.rect.x += self.speed
-            #сдвинуться вправо
-        if keys_pressed[K_LEFT] and self.rect.x >0:
-        #если нажата кнопка лево и координаты больше 0
-            self.rect.x -= self.speed
-            #сдвинуться влево
+        if keys_pressed[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed
+    def updateL(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 400:
+            self.rect.y += self.speed
+
+racket1 = Player("platformA.png", 20, 20, 5, 10, 70)
+racket2 = Player("platformB.png", 880, 20, 5, 10, 70)
+ball = GameSprite("ball.png", 400, 100, 50, 50, 50)
+finish = False
+game = True
+speed_x = 3
+speed_y = 3
+while game:
+    
+    
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.blit(background, (0, 0))
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        if ball.rect.y > 400:
+            speed_y *= -1
+        if ball.rect.y < 0:
+            speed_y *= -1
+        
+
+
+        racket1.updateL()
+        racket1.reset()
+        racket2.update()
+        racket2.reset()
+        ball.update()
+        ball.reset()
+
+    display.update()
+    time.delay(10)
